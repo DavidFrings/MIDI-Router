77eca5f51519e422f83e72908d031df9aef00bac
David Frings, aka DevOpsDavid <dev@davidfrings.dev>
2025-09-22 18:25:21 +0200

    fix: run -> build

--- Build&Install.cmd
+++ Build&Install.cmd
@@ -1,4 +1,4 @@
-cargo run --release
+cargo build --release
 mkdir "C:\Program Files\David Frings\"
 copy ".\target\release\apc40mk2-router.exe" "C:\Program Files\David Frings\"
 copy ".\target\release\updater.exe" "C:\Program Files\David Frings\"
